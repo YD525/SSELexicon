a1c5b3dacae99f1ab38966ad76e2f4629af8334b
YD525 <525084464@qq.com>
2025-08-20 03:48:16 +0800

    UPDate

---
+++ SSELex/FileManagement/ExplorerHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSELex.FileManagement
+{
+    public class ExplorerHelper
+    {
+        public static bool OpenUrl(string Url)
+        {
+            try
+            {
+                var PSI = new ProcessStartInfo
+                {
+                    FileName = Url,
+                    UseShellExecute = true  //Let the operating system decide which program to open
+                };
+
+                Process.Start(PSI);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public static bool SelectFile(string FilePath)
+        {
+            try
+            {
+                var PSI = new ProcessStartInfo
+                {
+                    FileName = "explorer.exe",
+                    Arguments = "/select,\"" + FilePath + "\""
+                };
+                Process.Start(PSI);
+
+                return true;
+            }
+            catch(Exception e) 
+            {
+                return false;
+            }
+        }
+    }
+}
