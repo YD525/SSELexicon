ed896866609a5afa81b5dc86af12ec154036ee46
YD525 <525084464@qq.com>
2025-11-05 04:34:49 +0800

    v3.1.2.5

--- SSELex/DeFine.cs
+++ SSELex/DeFine.cs
@@ -40,7 +40,7 @@ namespace SSELex
 
         public static string BackupPath = @"\BackUpData\";
 
-        public static string CurrentVersion = "3.1.1.3";
+        public static string CurrentVersion = "3.1.2.5";
         public static LocalSetting GlobalLocalSetting = new LocalSetting();
 
         public static MainGui WorkingWin = null;
