eada321562899fb2b7795bedb5b56eb73726105d
约定 <44013068+YD525@users.noreply.github.com>
2025-12-26 14:23:41 +0800

    Update TranslationPreprocessorExtend.cs

--- SSELex/TranslateManagement/TranslationPreprocessorExtend.cs
+++ SSELex/TranslateManagement/TranslationPreprocessorExtend.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace SSELex.TranslateManagement
 {
     // Copyright (C) 2025 YD525
-    // Licensed under the GNU GPLv3
+    // Licensed under the CC BY-NC-ND 4.0
     // See LICENSE for details
     //https://github.com/YD525/PhoenixEngine
     public class TranslationPreprocessorExtend
